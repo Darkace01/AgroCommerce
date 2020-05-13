db9857a26fa8a755d65fe82046d7241881359ebd
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-05-13 19:36:00 +0100

    Scarfolding Identity Into Project

---
+++ AgroCommerce/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AgroCommerce.Core;
+using AgroCommerce.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AgroCommerce.Areas.Identity.IdentityHostingStartup))]
+namespace AgroCommerce.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
