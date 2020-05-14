a5298e345037fde1c8ec5c8478cbf177a965622a
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-05-14 19:14:55 +0100

    Create Farm

--- AgroCommerce/Controllers/SellerController.cs
+++ AgroCommerce/Controllers/SellerController.cs
@@ -1,16 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using AgroCommerce.Core;
+using AgroCommerce.Services.Contracts;
+using AgroCommerce.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgroCommerce.Controllers
 {
     public class SellerController : Controller
     {
+        private readonly IUserAccountService _userAccountService;
+        private readonly IFarmService _farmService;
+        private readonly IListingService _listingService;
+        private readonly IAnimalTypeService _animalTypeService;
+        private readonly ITransactionService _transactionService;
+        private readonly IReviewService _reviewService;
+        public SellerController(IUserAccountService userAccountService, IFarmService farmService, IListingService listingService, IAnimalTypeService animalTypeService, ITransactionService transactionService, IReviewService reviewService)
+        {
+            _userAccountService = userAccountService;
+            _farmService = farmService;
+            _listingService = listingService;
+            _animalTypeService = animalTypeService;
+            _transactionService = transactionService;
+            _reviewService = reviewService;
+        }
         public IActionResult Index()
         {
+            ViewBag.Farm = _farmService.GetAll().Count();
             return View();
         }
+
+        public IActionResult SetUpFarm()
+        {
+            var user = GetLoggedInUser();
+
+            FarmAddViewModel farm = new FarmAddViewModel();
+            farm.ImagePath = "https://image.freepik.com/free-vector/farmer-peasant-illustration-man-with-beard-spade-farmland_33099-575.jpg";
+            return View(farm);
+        }
+
+        [HttpPost]
+        public IActionResult SetUpFarm(FarmAddViewModel farm)
+        {
+            var user = GetLoggedInUser();
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if(farm.ImagePath != null)
+                    {
+                        farm.ImagePath = "https://image.freepik.com/free-vector/farmer-peasant-illustration-man-with-beard-spade-farmland_33099-575.jpg";
+                    }
+
+                    Farm _farm = new Farm()
+                    {
+                        Name = farm.Name,
+                        Location = farm.Location,
+                        ImagePath = "https://image.freepik.com/free-vector/farmer-peasant-illustration-man-with-beard-spade-farmland_33099-575.jpg",
+                        ApplicationUserId = user.Id,
+                        IsActive = true,
+                    };
+                    _farmService.SetupFarm(_farm, user);
+                }
+                else
+                {
+                    return View(farm);
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists contact the administrator."+ex);
+                throw;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        #region Helper Methods
+        private ApplicationUser GetLoggedInUser()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return _userAccountService.GetById(userId);
+        }
+        #endregion
     }
 }
---
+++ AgroCommerce/ViewModels/FarmViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgroCommerce.ViewModels
+{
+    public class FarmAddViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
