8765676022adc8eb01b45ff987c6bc237c1d81c2
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-05-14 17:34:17 +0100

    Fetching the product from the database

--- AgroCommerce/Controllers/HomeController.cs
+++ AgroCommerce/Controllers/HomeController.cs
@@ -5,13 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AgroCommerce.Models;
+using AgroCommerce.Services.Contracts;
 
 namespace AgroCommerce.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IListingService _listingService;
+        private readonly IAnimalTypeService _animalTypeService;
+        private readonly IUserAccountService _userAccountService;
+
+        public HomeController(IListingService listingService, IAnimalTypeService animalTypeService, IUserAccountService userAccountService)
+        {
+            this._listingService = listingService;
+            this._animalTypeService = animalTypeService;
+            this._userAccountService = userAccountService;
+        }
         public IActionResult Index()
         {
+            ViewBag.AnimalTypes = _animalTypeService.GetAll();
+            ViewBag.ActiveFeaturedList = _listingService.GetAllWithImage().FirstOrDefault();
+            ViewBag.FeaturedList = _listingService.GetAllWithImage().Skip(1).Take(5);
+            ViewBag.AllListings = _listingService.GetAllWithImage().Skip(4);
+            ViewBag.Chickens = _listingService.GetAllWithImage().Where(l => String.Compare(l.AnimalType, "Chicken", true) == 0);
+            ViewBag.Cows = _listingService.GetAllWithImage().Where(l => String.Compare(l.AnimalType, "Cow", true) == 0);
+            ViewBag.Fishes = _listingService.GetAllWithImage().Where(l => String.Compare(l.AnimalType, "Fish", true) == 0);
+            ViewBag.Goats = _listingService.GetAllWithImage().Where(l => String.Compare(l.AnimalType, "Goat", true) == 0);
+            ViewBag.Horses = _listingService.GetAllWithImage().Where(l => String.Compare(l.AnimalType, "Horse", true) == 0);
+            ViewBag.Pigs = _listingService.GetAllWithImage().Where(l => String.Compare(l.AnimalType, "Pig", true) == 0);
             return View();
         }
 
