90a75895e3380f8239968b13e0d2421dc2feafdb
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-05-29 14:45:26 +0100

    Custom Exception Added : File Save Exception

---
+++ AgroCommerce.Utilities/CustomExceptions/FileException.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgroCommerce.Utilities.CustomExceptions
+{
+    [Serializable]
+    public class FileSaveErrorException : Exception
+    {
+        public FileSaveErrorException()
+        {
+        }
+
+        public FileSaveErrorException(string message)
+            : base(message)
+        {
+        }
+
+        public FileSaveErrorException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    [Serializable]
+    public class NoFileFoundException : Exception
+    {
+        public NoFileFoundException()
+        {
+        }
+
+        public NoFileFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public NoFileFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+
+
+    [Serializable]
+    public class FileNotAnImageException : Exception
+    {
+        public FileNotAnImageException()
+        {
+        }
+
+        public FileNotAnImageException(string message)
+            : base(message)
+        {
+        }
+
+        public FileNotAnImageException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    [Serializable]
+    public class FileTooLargeException : Exception
+    {
+        public FileTooLargeException()
+        {
+        }
+
+        public FileTooLargeException(string message)
+            : base(message)
+        {
+        }
+
+        public FileTooLargeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
