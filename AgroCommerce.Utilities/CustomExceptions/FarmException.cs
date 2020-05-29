3d018c169bb7f92a5c2d938e4854e78434c5a0c4
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-05-29 19:40:17 +0100

    Farm Exception

---
+++ AgroCommerce.Utilities/CustomExceptions/FarmException.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgroCommerce.Utilities.CustomExceptions
+{
+    [Serializable]
+    public class FarmSaveErrorException : Exception
+    {
+        public FarmSaveErrorException()
+        {
+        }
+
+        public FarmSaveErrorException(string message)
+            : base(message)
+        {
+        }
+
+        public FarmSaveErrorException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    [Serializable]
+    public class FarmDeleteErrorException : Exception
+    {
+        public FarmDeleteErrorException()
+        {
+        }
+
+        public FarmDeleteErrorException(string message)
+            : base(message)
+        {
+        }
+
+        public FarmDeleteErrorException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    [Serializable]
+    public class NoFarmFoundException : Exception
+    {
+        public NoFarmFoundException()
+        {
+        }
+
+        public NoFarmFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public NoFarmFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
